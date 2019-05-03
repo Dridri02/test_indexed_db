9fb0a6d1f728ef599360e5be5acf46c4a4fb5bab
Adrien Montbroussous <adrien.montbroussous@insee.fr>
2019-05-03 11:17:40 +0200

    config sw create react app

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,13 @@
 console.log("Service worker running!");
+
+self.addEventListener("install", function() {
+  console.log("Install!");
+});
+
+self.addEventListener("activate", function() {
+  console.log("Activate!");
+});
+
+self.addEventListener("fetch", function(event) {
+  console.log("Fetch!", event.request);
+});
